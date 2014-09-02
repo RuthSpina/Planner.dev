a9f2246f35b967e7b92ff047fb1348dba993b1de
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-09-02 08:48:21 -0500

    'update' 'update' 'update' 'update'

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Page on Planner.dev</title>
+	<title>To Do List</title>
+	<link rel="stylesheet" href="css/todo.css"/>
 </head>
 <body>
-<p>Hello World!</p>
+<div id = "header">	
+<h3>Post Data:</h3>	
+<?php var_dump($_POST); ?>
+<br>
+<h3>Get Data:</h3>
+<?php var_dump($_GET); ?>
+<br>
+</div>
+<hr>
+<h1>To Do List:</h1>
+<table>
+	<ul>
+		<li>Walk the dog.</li>
+		<li>Take out the trash.</li>
+		<li>Mow the lawn.</li>		
+	</ul>
+</table>
+
+
+<form>
+
+<h1>To Do List Entry Form:</h1>
+ <form method="POST" action="todo_list.php">
+
+ <label for="new_item">New Item:</label>
+        <input id="new_item" name="new_item" type="text" placeholder ="Enter New Item"><br><br>
+
+        <input type="submit">
+
+</form>		
 </body>
 </html>	
