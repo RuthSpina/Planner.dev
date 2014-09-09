6d191b43175b70f42a15e715f872e039ce6e63fe
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-09-09 10:33:34 -0500

    'updated file' 'initial commit' 'updated to add superglobals'

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,40 +1,63 @@
 <!DOCTYPE html>
-<html>
+<?php
+//$items= ['Walk the dog', 'Take out the trash', 'Mow the lawn'];
+define('FILENAME', 'data/data1.txt');
 
+
+// update list
+   function open_file($file_input = FILENAME){
+    $handle = fopen($file_input, 'r');
+    $content = trim(fread($handle, filesize($file_input)));
+    $new_list = explode("\n", $content);
+    fclose($handle);
+    return $new_list;
+    }
+
+
+//save list to filename
+    function save_to_file($items, $file_path = FILENAME){
+        $handle = fopen($file_path, 'w');
+        foreach($items as $item){
+        fwrite($handle, $item.PHP_EOL);
+        }
+
+        fclose($handle);
+    }
+    $items = [];
+    $items = open_file();
+
+?>
+<html>
 <head>
 	<title>To Do List</title>
 	<link rel="stylesheet" href="css/todo.css"/>
 </head>
 <body>
-<div id = "header">	
-<h3>Post Data:</h3>	
-<?php var_dump($_POST); ?>
-<br>
-<h3>Get Data:</h3>
-<?php var_dump($_GET); ?>
-<br>
-</div>
-<hr>
 <h1>To Do List:</h1>
-<table>
-	<ul>
-		<li>Walk the dog.</li>
-		<li>Take out the trash.</li>
-		<li>Mow the lawn.</li>		
-	</ul>
-</table>
+    <ul>
+    <?php
+        if (isset($_POST['new_item'])){
+    $items[]= $_POST['new_item'];
+    save_to_file($items);
+    }
+    if(isset($_GET['remove'])){
+     $keyToRemove=$_GET['remove'];
+     unset($items[$keyToRemove]);
+     $items = array_values($items);
+     save_to_file($items);
+    }
 
-
-<form>
-
-<h1>To Do List Entry Form:</h1>
- <form method="POST" action="todo_list.php">
-
+    foreach ($items as $key => $item) {
+    echo '<li> <a href=' . "?remove=$key" .'>Complete</a>-'. "$item</li>";
+    }
+    ?>
+    </ul>
+<h1>New Item:</h1>
+ <form name= "additem" method="POST" action="/todo_list.php">
  <label for="new_item">New Item:</label>
-        <input id="new_item" name="new_item" type="text" placeholder ="Enter New Item"><br><br>
+        <input id="new_item" name="new_item" type="text" placeholder ="Enter New Item">
 
         <input type="submit">
-
-</form>		
+</form>
 </body>
-</html>	
+</html>
